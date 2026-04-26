110b5209e09e2476c03ab658cb3f370891bc7531
Zuri McKee <zurimckee95@gmail.com>
2026-04-26 01:17:07 -0400

    deep sigh

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import SmartPlusParkingApp from './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SmartPlusParkingApp />
+    <App />
   </StrictMode>,
 )
